16c2fe47fe4b86c2335dc06dd22283e2aba9f787
Roharui <jack555586@gmail.com>
2023-01-19 22:48:04 +0900

    init: initial commit

---
+++ src/deps.ts
@@ -0,0 +1,1 @@
+export * from "https://deno.land/x/harmony@v2.8.0/mod.ts";
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// Importing Client and Intents class from Harmony
+import { Client, Intents } from "@/deps.ts";
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+}
